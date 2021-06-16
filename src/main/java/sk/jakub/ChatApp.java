09d43dcd82304b57945b859237261b755b83adfd
JakubMihalik <xmihalikj1@stuba.sk>
2021-06-16 21:12:57 +0200

    Initial commit

---
+++ src/main/java/sk/jakub/ChatApp.java
@@ -0,0 +1,9 @@
+package sk.jakub;
+
+import sk.jakub.utilities.ui.Window;
+
+public class ChatApp {
+    public static void main(String[] args) {
+        new Window();
+    }
+}
---
+++ src/main/java/sk/jakub/utilities/ui/Window.java
@@ -0,0 +1,16 @@
+package sk.jakub.utilities.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+
+    public Window() {
+        super("ChatApp [Beta]");
+        setExtendedState(MAXIMIZED_BOTH);
+        setMinimumSize(new Dimension(500, 700));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+}
